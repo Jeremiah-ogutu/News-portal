511d1540f064a993709641299e6b60d63d40b2cd
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-19 22:34:49 +0300

    add equals

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -33,33 +33,33 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
-//    }
-//
-//    public void setUserName(String userName) {
-//        UserName = userName;
-//    }
-//
-//    public void setPhone_no(String phone_no) {
-//        this.phone_no = phone_no;
-//    }
-//
-//    public void setPosition(String position) {
-//        this.position = position;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof User)) return false;
-//        User user = (User) o;
-//        return id == user.id &&
-//                UserName.equals(user.UserName) &&
-//                phone_no.equals(user.phone_no) &&
-//                position.equals(user.position);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, UserName, phone_no, position);
-//    }
-//}
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id &&
+                UserName.equals(user.UserName) &&
+                phone_no.equals(user.phone_no) &&
+                position.equals(user.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, UserName, phone_no, position);
+    }
+}
