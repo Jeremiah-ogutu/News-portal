11dd793df1a086b3064c00d7f75b3b67af540194
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-19 22:44:53 +0300

    Department strings

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,7 +1,18 @@
 package models;
 
 public class Department {
-    private  String department;
+//    private  int id;
+    private  String department_name;
     private String No_of_Employees;
+    private  String description;
+
+
+    public  Department( String department_name, String no_of_employees, String description) {
+        this.department_name= department_name;
+        this.description=description;
+        this.No_of_Employees=no_of_employees;
+    }
+
+
 
 }
